b5c142ba80a989593be53f4e4bb50b3a8b7f35a2
staticsafe <sadiq.9541@gmail.com>
2012-01-05 13:28:53 -0500

    fu indents

--- envsetup.py
+++ envsetup.py
@@ -35,16 +35,20 @@ def urldownload(confurl = ""):
 
 	file_size_dl = 0
 	block_sz = 8192
+
 	while True:
     	buffer = u.read(block_sz)
     	if not buffer:
         	break
+
     	file_size_dl += len(buffer)
     	f.write(buffer)
     	status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
     	status = status + chr(8)*(len(status)+1)
     	print status,
+
 	f.close()
+
 
 def confdownload():
 	urldownload(confurl = vimrcurl)
