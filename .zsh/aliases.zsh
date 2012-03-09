7bbee8c8a7609ecf1885b7327c18367be2435fe8
staticsafe <sadiq.9541@gmail.com>
2012-03-08 20:55:07 -0500

    fixing macgen alias (quotes)

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -3,4 +3,4 @@ alias sl='ls'
 alias myip="wget -qO- ifconfig.me/ip"
 alias memoryhog="ps aux | sort -nk +4 | tail"
 alias stripext="ls -1 | sed 's/\(.*\)\..*/\1/'"
-alias macgen="openssl rand -hex 6 | sed 's/\(..\)/\1:/g;s/../00/;s/.$//' | tr "[a-z]" "[A-Z]""
+alias macgen="openssl rand -hex 6 | sed 's/\(..\)/\1:/g;s/../00/;s/.$//' | tr '[a-z]' '[A-Z]'"
