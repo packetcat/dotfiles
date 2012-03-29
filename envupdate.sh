1b8bec5968b7e99db9e0b0740cc68a4d6a08fdf7
staticsafe <sadiq.9541@gmail.com>
2012-03-29 11:16:14 -0400

    fixing logical issues.

--- envupdate.sh
+++ envupdate.sh
@@ -11,18 +11,6 @@ exit 1
 
 cd
 
-# Update all the conf files here
-printf '%s\n' "Updating dotfiles now!"
-wget --no-check-certificate -O .zshrc https://raw.github.com/staticsafe/dotfiles/master/.zshrc || die 'Download failed!'
-wget --no-check-certificate -O .vimrc https://raw.github.com/staticsafe/dotfiles/master/.vimrc || die 'Download failed!'
-wget --no-check-certificate -O .tmux.conf https://raw.github.com/staticsafe/dotfiles/master/.tmux.conf || die 'Download failed!'
-wget --no-check-certificate -O .conkyrc https://raw.github.com/staticsafe/dotfiles/master/.conkyrc || die 'Download failed!'
-
-# vim dir
-rm -r ~/.vim
-wget -O vimdir.tar.bz2 http://dl.dropbox.com/u/2888062/vimdir.tar.bz2 || die 'Download failed!'
-tar xjvf vimdir.tar.bz2
-
 # zsh dir
 
 if [[ -d ~/dev/dotfiles ]]; then
@@ -40,3 +28,21 @@ else
     git clone https://github.com/staticsafe/dotfiles.git
     ln -s ~/dev/dotfiles/.zsh ~/
 fi
+
+
+# Update all the conf files here
+printf '%s\n' "Updating dotfiles now!"
+wget --no-check-certificate -O .zshrc https://raw.github.com/staticsafe/dotfiles/master/.zshrc || die 'Download failed!'
+wget --no-check-certificate -O .vimrc https://raw.github.com/staticsafe/dotfiles/master/.vimrc || die 'Download failed!'
+wget --no-check-certificate -O .tmux.conf https://raw.github.com/staticsafe/dotfiles/master/.tmux.conf || die 'Download failed!'
+wget --no-check-certificate -O .conkyrc https://raw.github.com/staticsafe/dotfiles/master/.conkyrc || die 'Download failed!'
+
+# vim dir
+rm -r ~/.vim
+wget -O vimdir.tar.bz2 http://dl.dropbox.com/u/2888062/vimdir.tar.bz2 || die 'Download failed!'
+tar xjvf vimdir.tar.bz2
+
+
+# cleanup
+rm ~/vimdir.tar.bz2
+
