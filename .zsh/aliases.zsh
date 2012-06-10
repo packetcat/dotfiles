5d662e628338bba93804b9eedf49dabeaecc4dd4
staticsafe <staticsafe@entropynet.net>
2012-06-10 08:34:12 -0400

    making ls use colour by default via alias.

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -7,3 +7,4 @@ alias macgen="openssl rand -hex 6 | sed 's/\(..\)/\1:/g;s/../00/;s/.$//' | tr '[
 alias quickpwgen="tr -cd '[:alnum:]' < /dev/urandom | fold -w30 | head -n1"
 alias mailstop="sudo /etc/init.d/postfix stop && sudo /etc/init.d/dovecot stop && sudo /etc/init.d/amavis stop && sudo /etc/init.d/mysql stop && sudo /etc/init.d/saslauthd stop && sudo /etc/init.d/spamassassin stop"
 alias mailstart="sudo /etc/init.d/postfix start && sudo /etc/init.d/dovecot start && sudo /etc/init.d/amavis start && sudo /etc/init.d/mysql start && sudo /etc/init.d/saslauthd start && sudo /etc/init.d/spamassassin start"
+alias ls="ls --color=auto"
