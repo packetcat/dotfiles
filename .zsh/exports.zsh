6c550ab9a7592a90803056ac0e8ebd6bc91090c5
staticsafe <staticsafe@entropynet.net>
2012-06-10 08:58:44 -0400

    modifying ls alias to be more *nix friendly and adding grep options

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -1,10 +1,13 @@
 #default apps
 export EDITOR="vim"
 export PAGER="most"
-#history stuff
+# history stuff
 # number of lines kept in history
 export HISTSIZE=1000
 # number of lines saved in the history after logout
 export SAVEHIST=1000
 # location of history
 export HISTFILE=~/.zhistory
+# grep options
+export GREP_OPTIONS='--color=auto'
+export GREP_COLOR='1;32'
