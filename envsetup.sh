28cad15b17466f24ce0b48b488eca3eda16cbfcd
staticsafe <staticsafe@entropynet.net>
2012-06-16 14:41:51 -0400

    ugh

--- envsetup.sh
+++ envsetup.sh
@@ -14,7 +14,7 @@ die() {
 userdistro="NULL"
 repo="git://github.com/staticsafe/dotfiles.git"
 
-portpath == "/opt/local/bin/port" # This is the default install directory for the MacPorts port binary, you may find this useful.
+portpath="/opt/local/bin/port" # This is the default install directory for the MacPorts port binary, you may find this useful.
 
 # Lets get this party on the road, shall we?
 
