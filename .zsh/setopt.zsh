81ad26322f7f06c7d15d14ee381cf5da814b4a8f
staticsafe <staticsafe@entropynet.net>
2012-08-16 21:18:26 -0400

    add some additional setopts

--- .zsh/setopt.zsh
+++ .zsh/setopt.zsh
@@ -12,6 +12,14 @@ setopt EXTENDED_HISTORY
 setopt EXTENDED_GLOB
 setopt AUTO_CD
 
+# display PID when suspending processes as well
+setopt longlistjobs
+# try to avoid the 'zsh: no matches found...'
+setopt nonomatch
 # Turn off flow control
 setopt noflowcontrol
-
+# report the status of backgrounds jobs immediately
+setopt notify
+# whenever a command completion is attempted, make sure the entire command path
+# is hashed first.
+setopt hash_list_all
