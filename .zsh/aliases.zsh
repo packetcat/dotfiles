cb12818bf3d932aaa3b6709bdd90bf9b9fda0d1e
staticsafe <staticsafe@entropynet.net>
2012-09-05 17:15:25 -0400

    modified mail aliases for new setup and added sprunge alias.

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -5,8 +5,9 @@ alias memoryhog="ps aux | sort -nk +4 | tail"
 alias stripext="ls -1 | sed 's/\(.*\)\..*/\1/'"
 alias macgen="openssl rand -hex 6 | sed 's/\(..\)/\1:/g;s/../00/;s/.$//' | tr '[a-z]' '[A-Z]'"
 alias quickpwgen="tr -cd '[:alnum:]' < /dev/urandom | fold -w30 | head -n1"
-alias mailstop="sudo /etc/init.d/postfix stop && sudo /etc/init.d/dovecot stop && sudo /etc/init.d/amavis stop && sudo /etc/init.d/mysql stop && sudo /etc/init.d/saslauthd stop && sudo /etc/init.d/spamassassin stop"
-alias mailstart="sudo /etc/init.d/postfix start && sudo /etc/init.d/dovecot start && sudo /etc/init.d/amavis start && sudo /etc/init.d/mysql start && sudo /etc/init.d/saslauthd start && sudo /etc/init.d/spamassassin start"
+alias mailstop="sudo /etc/init.d/postfix stop && sudo /etc/init.d/dovecot stop && sudo /etc/init.d/amavis stop && sudo /etc/init.d/spamassassin stop"
+alias mailstart="sudo /etc/init.d/postfix start && sudo /etc/init.d/dovecot start && sudo /etc/init.d/amavis start && sudo /etc/init.d/spamassassin start"
+alias sprunge="curl -F 'sprunge=<-' http://sprunge.us"
 
 if [ "$DISABLE_LS_COLORS" != "true" ]
 then
